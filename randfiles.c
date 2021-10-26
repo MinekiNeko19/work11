68a72d5d62029ba2d8f92fd41ad45b9f7527cde1
Sarah Leong <sleong20@stuy.edu>
2021-10-26 11:16:47 -0400

    populates an array

--- randfiles.c
+++ randfiles.c
@@ -3,6 +3,15 @@
 #include <time.h>
 
 int main() {
-    
+    srand(time(NULL));
+    int r[10];
+    printf("Generating random numbers: \n");
+    for (int i = 0; i<10; i++) {
+        r[i] = rand();
+        printf("\tr[%d]: %d\n",i,r[i]);
+    }
+
+
+
     return 0;
 }
