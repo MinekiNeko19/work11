c64c41fbeb23b248e0f6fdf7bd93f876f2d37959
Sarah Leong <sleong20@stuy.edu>
2021-10-26 22:26:29 -0400

    there's certainly something in store.txt

--- randfiles.c
+++ randfiles.c
@@ -16,34 +16,23 @@ int randomNum() {
 
 int main() {
     int r[10];
-    printf("testing randomNum: %d\n", randomNum());
+    // printf("testing randomNum: %d\n", randomNum());
     printf("Generating random numbers:\n");
     int i;
     for (i = 0; i < 10; i++) {
         r[i] = randomNum();
         printf("\tr[%d]: %d\n",i,r[i]);
     }
-    // r[0] = randomNum();
-    // printf("\tr[%d]: %d\n",0,r[0]);
-    // r[1] = randomNum();
-    // printf("\tr[%d]: %d\n",1,r[1]);
-    // r[2] = randomNum();
-    // printf("\tr[%d]: %d\n",2,r[2]);
-    // r[3] = randomNum();
-    // printf("\tr[%d]: %d\n",3,r[3]);
-    // r[4] = randomNum();
-    // printf("\tr[%d]: %d\n",4,r[4]);
-    // r[5] = randomNum();
-    // printf("\tr[%d]: %d\n",5,r[5]);
-    // r[6] = randomNum();
-    // printf("\tr[%d]: %d\n",6,r[6]);
-    // r[7] = randomNum();
-    // printf("\tr[%d]: %d\n",7,r[7]);
-    // r[8] = randomNum();
-    // printf("\tr[%d]: %d\n",8,r[8]);
-    // r[9] = randomNum();
-    // printf("\tr[%d]: %d\n",9,r[9]);
-    
+
+    printf("writing numbers into file\n");
+    int store = open("store.txt",O_WRONLY | O_APPEND | O_CREAT, 0644);
+    write(store,r,10*sizeof(int));
+
+    printf("reading numbers from file\n");
+    int new[10];
+
+
+    close (store);
 
     return 0;
 }
