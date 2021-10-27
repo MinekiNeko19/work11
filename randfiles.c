ed0083aef53a298011ca0aa3d9734f2e42bb4a13
Sarah Leong <sleong20@stuy.edu>
2021-10-26 21:20:52 -0400

    made the file that returns a random number

--- randfiles.c
+++ randfiles.c
@@ -6,13 +6,25 @@
 #include <fcntl.h>
 #include <string.h>
 
+int randomNum() {
+    int file = open("dev/random",O_RDONLY,0);
+    int * num;
+    read(file,num,sizeof(int));
+    close(file);
+    return *num;
+}
+
 int main() {
     int r[10];
-    int * p = r;
-    open("dev/random",O_RDONLY,0);
-    read("dev/random",r[0],8);
-    printf("%d\n",r[0]);
-    close("dev/random");
+    printf("testing randomNum: %d\n", randomNum());
+    // open(dev/random,O_RDONLY,0);
+    // printf("Generating random numbers:\n");
+    // for (int i = 0; i < 10; i++) {
+    //     read(dev/random,r[i],8);
+    //     printf("\tr[%d]: %d\n",i,r[i]);
+    // }
+    // printf("%d\n",r[0]);
+    // close(dev/random);
 
     return 0;
 }
